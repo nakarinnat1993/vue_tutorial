08ca34dce636c5f528549e073a06d95cafcc8197
unknown <nakarinnat1993@gmail.com>
2020-12-19 19:52:05 +0700

    Click Event And V-model

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <h1>{{ title }}</h1>
+    <h2>{{ count }}</h2>
+    <button @click="onClickAdd">Add</button>
+    <hr>
+    <h1>V-model</h1>
+    <input type="text" v-model="msg" />
+    <div>{{ msg }}</div>
+    <button @click="msg=''">Reset</button>
   </div>
-  <router-view/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  data() {
+    return {
+      title: "Click Event And V-model",
+      count: 0,
+      msg:""
+    };
+  },
+  methods: {
+    onClickAdd() {
+      this.count = this.count + 1;
+    },
+  },
+};
+</script>
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
