1d0e5c96b147bb9e49ba85f440915c86506d48cc
unknown <nakarinnat1993@gmail.com>
2020-12-25 19:06:19 +0700

    Components Header

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,20 @@
 <template>
-  <div>
-    <ul>
-      <li><router-link to="/"><button>Home</button></router-link></li>
-      <li><router-link to="/eventAndMethode"><button>Event & Method</button></router-link></li>
-      <li><router-link to="/data"><button>Data & Variables</button></router-link></li>
-      <li><router-link to="/computed"><button>Computed</button></router-link></li>
-      <li><router-link to="/binding"><button>Binding</button></router-link></li>
-      <li><router-link to="/form"><button>Form</button></router-link></li>
-    </ul>
-    
+  <div class="app">
+    <Header/>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Header from "@/components/layouts/Header.vue";
 
+export default {
+  name: "app",
+  components:{
+    Header
+  }
+};
 </script>
 
-<style scoped>
-ul li {
-  display:inline;
-  padding-right: 5px;
-}
+<style>
 </style>
